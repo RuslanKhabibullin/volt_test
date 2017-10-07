f6052a93dc77b7b3385c61c77d9d1b4eddca29e0
RuslanKhabibullin <khabibullin.ruslan.95@gmail.com>
2017-10-07 17:50:06 +0300

    add secret_key for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,8 @@ Devise.setup do |config|
 
   require "devise/orm/active_record"
 
+  # config.secret_key = ''
+
   config.case_insensitive_keys = [:email]
 
   config.strip_whitespace_keys = [:email]
